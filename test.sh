515b006a6fc4734a2e135210dbd9081990a23118
narayanan <76954320+narayanatgit@users.noreply.github.com>
2026-01-10 11:44:43 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PORTS=({$start_port..$end_port})
+for i in "${PORTS[@]}"
+do
+  echo $i
+done
+
+
 echo "===== Concourse Params Test ====="
 echo "Device IP : $DEVICE_IP"
 echo "Username  : $USERNAME"

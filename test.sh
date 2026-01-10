73fc3ab58d2d0524b141f9d681feb434fa4f6779
narayanan <76954320+narayanatgit@users.noreply.github.com>
2026-01-10 11:08:34 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "===== Concourse Params Test ====="
+echo "Device IP : $DEVICE_IP"
+echo "Username  : $USERNAME"
+echo "Message   : $MESSAGE"
+echo "================================="
